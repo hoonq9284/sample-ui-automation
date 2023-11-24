59d66b85694535d2ee503e343fd990347185848c
유승훈 <a5750791@gmail.com>
2023-11-24 21:06:30 +0900

    Feat : 로그인 영역 UI is_displayed 시나리오 추가

--- pages/PageElements.py
+++ pages/PageElements.py
@@ -1,3 +1,8 @@
 # MainPage Elements
 naver_search_field = '//div[@class="search_group"]'
 account_container = '//div[@id="account"]'
+login_info_text = '//div[@id="account"]/div/p'
+login_button = '//div[@id="account"]/div/a'
+login_find_id = '//a[contains(text(), "아이디 찾기")]'
+login_find_password = '//a[contains(text(), "비밀번호 찾기")]'
+login_register = '//a[contains(text(), "회원가입")]'
